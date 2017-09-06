246aa423e2aa3af407d5ffed310495aab175017a
hp34th <hp34th@gmail.com>
2017-09-06 18:59:27 +0530

    Properties and Chainable methods

--- Greetr.js
+++ Greetr.js
@@ -12,7 +12,87 @@
 																//returning obj with new coz to avoid the user of lib to write new each time when he uses "G$()" instead of "new G$()"
 	}
 
-	Greetr.prototype = {}; // creating obj inside where we will put methods that i want to use inside my obj thats returned by my Greetr 
+
+	//below objects are not exposed to outside world untill we desire like --SearchFor:goto1
+	var supportedLangs = ['en', 'es'];
+
+	var greetings = { //not using arrays coz we wanna ref them by name/value pairs by the name of the property
+		en: 'Hello',
+		es: 'Hola'
+	};
+
+	var formalGreetings = {
+		en: 'Greetings Formal',
+		es: 'Saludos Formal'
+	};
+
+	var logMessages = { ///only for dev purpose
+		en: 'Logged in',
+		es: 'Inicio sesion'
+	};
+
+	// will we writing exposable *methods* inside this
+	// imagine methods inside this Greetr.init fn
+	// refer notes to understand deeply
+	Greetr.prototype = { 
+
+		fullName: function() {
+			return this.firstName + ' ' + this.lastName;
+		},
+
+		validate: function() {
+			if (supportedLangs.indexOf(this.language) === -1) {
+				throw "Invalid language"
+			}
+		},
+
+		greeting: function() {
+			return greetings[this.language] + ' ' + this.firstName + '!';
+		},
+
+		formalGreeting: function() {
+			return formalGreetings[this.language] + ' ' + this.firstName + '!';
+		},
+
+		greet: function(formal) { // to make chainable methods like jq
+			var msg;
+
+			//if undefined or null it will be coerced to 'false'
+			if (formal) {
+				msg = this.formalGreeting();
+			}
+			else {
+				msg = this.greeting();
+			}
+
+			if (console) {
+				console.log(msg);
+			}
+
+			// 'this' refers to the calling oj at execution time
+			// makes the method chainable
+			return this;
+		},
+
+		log: function() {
+			if (console) {
+				console.log(logMessages[this.language] + ': ' + this.fullName() );
+			}
+
+			return this; //makes it chainable
+		},
+
+		setLang: function(lang) {
+			this.language = lang;
+
+			this.validate();
+
+			return this;
+		}   
+
+
+
+	}; // creating obj inside where we will put methods that i want to use inside my obj thats returned by my Greetr 
 
 	// Now we are building a function constructor that builts an obj which gives 3 properties and sets its value 
 
@@ -28,9 +108,10 @@
 
 	}
 
-	Greetr.init.prototype = Greetr.prototype;
+	Greetr.init.prototype = Greetr.prototype; // this makes it possible that Greetr.init have access to all the methods that are present in Greetr.prototype obj
 
 	global.Greetr = global.G$ = Greetr; // Exposing Greetr to global obj so that we can use our own "G$" 
+										// --here: goto1
 
 
 }(window, jQuery)); // invoking this function by passing window and jQuery/$ 
--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 var g = G$('john', 'doe'); //returns object like jquery
-console.log(g);
+//	g.greet(); // o/p: Hello john!
+//	g.greet().greet(true); // o/p: Greetings Formal john!
+							// "g.greet()" returns "this" viz pointing to g which then "g.greet(true)" is invoked
+g.greet().setLang('es').greet(true); // o/p: Saludos Formal john!							
+//	g.greet().setLang('japanese').greet(true); // will show our error 
